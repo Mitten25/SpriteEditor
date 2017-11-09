e120218aad4750f02c3240dd4c227df3087485c3
LINGXI-XPS\Lingxi <lingxizhong@gmail.com>
2017-11-09 11:21:09 -0700

    Caching bryans changes for him to prep for merge conflicts

--- frame.h
+++ frame.h
@@ -3,21 +3,27 @@
 #include <QVector>
 #include <QColor>
 #include <QTableWidget>
+#include <QString>
 #include <tuple>
 
 class Frame
 {
 
 private:
-    int height;
-    int width;
+    int row;
+    int column;
 
 public:
     QVector <QVector<std::tuple<int, int, int, int>>> pixels;
+    Frame(int height, int width);
     Frame();
-    std::tuple<int, int, int, int> getPixel();
-    void setPixel(std::tuple <int, int, int, int>);
+    std::tuple<int, int, int, int> getPixel(int x, int y);
+    void setPixel(int x, int y);
+    void setColor(std::tuple <int, int, int, int> c);
 	static Frame fromTableWidget(QTableWidget* tableWidget);
+    QString toString();
+
+    std::tuple <int, int, int, int> color;
 };
 
 #endif // FRAME_H
